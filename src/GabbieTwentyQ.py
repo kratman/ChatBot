baa9229ae3f7aaa406623d9f105fd7e1b286402d
kratman <kratmandu@gmail.com>
2017-10-07 22:11:17 -0400

    Creating a twenty questions class

---
+++ src/GabbieTwentyQ.py
@@ -0,0 +1,33 @@
+##############################################################################
+#                                                                            #
+#                        GabbieBot: A simple chat bot                        #
+#                              By Eric G. Kratz                              #
+#                                                                            #
+##############################################################################
+
+# Python class to play twenty questions
+
+### Hard-coded settings ###
+
+
+
+### Debug settings ###
+
+
+
+### Import libraries ###
+
+
+### Twenty question class ###
+
+class TwentyQuest:
+  # Constructor
+  def __init_(self):
+    self.answers = {}
+    return
+  # Setup function
+  def readMemories(self):
+    return
+  # Main routine
+  def play(self):
+    return
