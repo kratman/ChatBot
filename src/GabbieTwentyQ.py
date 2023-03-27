841d918f33539a5bdc01d27a61d125d6742a432f
kratman <kratmandu@gmail.com>
2023-03-26 20:31:12 -0400

    Add basic machinery for 20 questions

--- src/GabbieTwentyQ.py
+++ src/GabbieTwentyQ.py
@@ -7,28 +7,52 @@
 
 # Python class to play twenty questions
 
+import random
+
+
 class TwentyQuest:
-    # Constructor
-    def __init_(self):
-        self.knownAnswers = {}
-        self.questions = {}
-        self.userAnswers = []
-        return
+    numberOfQuestions = 20
+    knownAnswers = {}
+    questions = []
+    usedQuestions = []
+    userAnswers = []
 
-    # Setup function
-    def readMemories(self, memPath):
-        return
-
-    # Learning functions
-    def newAnswer(self, newKey):
-        return
-
-    def updatedAnswer(self, oldKey):
-        return
-
-    def updateMemories(self, newMemory):
-        return
-
-    # Main routine
     def play(self):
+        count = 0
+        while count < self.numberOfQuestions:
+            nextQuestion = self.selectQuestion()
+            self.askQuestion(nextQuestion)
+            self.userAnswers.append(self.getResponse())
+        guess = self.makeGuess()
+        if not self.guessIsCorrect(guess):
+            self.updateMemories()
         return
+
+    def randomQuestion(self):
+        return random.choice(self.questions)
+
+    def selectQuestion(self):
+        if len(self.questions) < self.numberOfQuestions:
+            raise RuntimeError(f"Fewer than {self.numberOfQuestions} are in defined.")
+        while (question := self.randomQuestion()) not in self.usedQuestions:
+            continue
+        return question
+
+    def askQuestion(self, nextQuestion):
+        self.usedQuestions.append(nextQuestion)
+        print(nextQuestion)
+
+    def makeGuess(self):
+        raise NotImplementedError
+
+    def getResponse(self):
+        raise NotImplementedError
+
+    def guessIsCorrect(self, guess):
+        raise NotImplementedError
+
+    def updateMemories(self):
+        raise NotImplementedError
+
+    def readMemories(self, memPath):
+        raise NotImplementedError
